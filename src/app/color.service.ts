9e15b3ddc31b05d0e2c467cb2af5491b2ca7974c
SVE974 <sve974@yahoo.fr>
2020-10-16 09:23:53 +0200

    Change detection partie 3 : Utilisation

---
+++ src/app/color.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { interval } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ColorService {
+
+  colors = [
+    "green",
+    "purple",
+    "yellow",
+    "blue",
+    "orange"
+  ]
+
+  constructor() { }
+
+  public randomColor() {
+    const max = Math.floor(this.colors.length - 1);
+    return this.colors[Math.floor(Math.random() * (max + 1))];
+  }
+
+  public get(){
+    return interval(1000).pipe(
+      map( _ => this.randomColor())
+    )
+  }
+}
--- src/app/race/race.component.ts
+++ src/app/race/race.component.ts
@@ -13,13 +13,6 @@ export class RaceComponent implements OnInit {
     {id: 3, color: "blue"},
     {id: 4, color: "yellow"}
   ]
-  colors = [
-    "green",
-    "purple",
-    "yellow",
-    "blue",
-    "orange"
-  ]
 
   constructor() { }
 
@@ -30,15 +23,4 @@ export class RaceComponent implements OnInit {
     console.log("Chargement du composant race")
   }
 
-  public changeColor() {
-    // this.ponies[0].color=this.randomColor(0, this.colors.length -1);
-    const pony = this.ponies[0];
-    this.ponies[0] = { ...pony, color: this.randomColor(0,this.colors.length -1)}
-  }
-
-  public randomColor(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return this.colors[Math.floor(Math.random() * (max - min + 1)) + min];
-  }
 }
